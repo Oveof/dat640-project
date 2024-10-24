8f3d290dd4dd7ae44a34cbd61ca95f942a4a7da4
oveof <ovedy@oftedal.com>
2024-10-24 23:26:54 +0200

    fix: genre

--- website/source/tools/get_song_genre.py
+++ website/source/tools/get_song_genre.py
@@ -1,4 +1,7 @@
 from langchain_core.tools import tool
+from sqlalchemy import select
+from sqlalchemy import func
+from sqlalchemy.orm import selectinload
 from typing import Annotated, List
 from source.db import Song, session_maker
 
@@ -15,7 +18,16 @@ def get_song_genre(song_name: Annotated[str, "name of song"])-> Annotated[List, 
 def db_get_song_genre(song_name: str) -> List:
     session = session_maker()
     search_pattern = f"%{song_name.lower()}%"
-    song = session.query(Song).filter_by(name=song_name).first()
+    stmt = (
+        select(Song)
+        .options(
+            selectinload(Song.artists),
+            selectinload(Song.albums)
+        )
+        .where(func.lower(Song.name).like(search_pattern))
+    )
+
+    song = session.execute(stmt).scalars().first()
     if song is None:
         return []
     genres = song.genres
